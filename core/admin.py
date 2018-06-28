1bc83b69b58621be27774b61a5f1ace00ba5a3a5
Aakash Das <aakashsjr@gmail.com>
2018-06-28 14:07:02 +0530

    login fix

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from core.models import *
+
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ("GrievanceApp", {'fields': ('projects', 'organisation', 'position')}),
+    )
+
 
 
 admin.site.register(Project)
 admin.site.register(ResponsibleEntity)
 admin.site.register(Grievance)
 admin.site.register(Case)
-admin.site.register(User)
+admin.site.register(User, CustomUserAdmin)
